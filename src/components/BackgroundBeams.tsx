2b10533cfe72861ccfc02757f500137cf5de53fa
Max Demian <max3demian@gmail.com>
2025-06-23 02:18:41 -0300

    Update BackgroundBeams.tsx

--- src/components/BackgroundBeams.tsx
+++ src/components/BackgroundBeams.tsx
@@ -2,34 +2,185 @@ import React from "react";
 
 const BackgroundBeams: React.FC = () => {
   return (
-    <div className="absolute inset-0 overflow-hidden bg-white z-0">
-      {/* Container das beams - linhas finas diagonais animadas */}
-      <div className="absolute inset-0 flex space-x-4 -rotate-12">
-        {[...Array(8)].map((_, i) => (
-          <div
-            key={i}
-            className="w-0.5 bg-gradient-to-b from-transparent via-gray-400 to-transparent opacity-20 blur-sm animate-beam-move"
-            style={{ animationDelay: `${i * 0.8}s` }}
+    <div className="absolute inset-0 bg-black overflow-hidden z-0">
+      <svg
+        className="w-full h-full"
+        xmlns="http://www.w3.org/2000/svg"
+        preserveAspectRatio="xMidYMid slice"
+        viewBox="0 0 1440 960"
+        fill="none"
+      >
+        <g filter="url(#filter0_f_4_216)">
+          <rect
+            x="-240"
+            y="0"
+            width="240"
+            height="1200"
+            fill="url(#paint0_linear_4_216)"
+            fillOpacity="0.3"
+            transform="rotate(30 -240 0)"
           />
-        ))}
-      </div>
+          <rect
+            x="0"
+            y="0"
+            width="240"
+            height="1200"
+            fill="url(#paint1_linear_4_216)"
+            fillOpacity="0.3"
+            transform="rotate(30 0 0)"
+          />
+          <rect
+            x="240"
+            y="0"
+            width="240"
+            height="1200"
+            fill="url(#paint2_linear_4_216)"
+            fillOpacity="0.3"
+            transform="rotate(30 240 0)"
+          />
+          <rect
+            x="480"
+            y="0"
+            width="240"
+            height="1200"
+            fill="url(#paint3_linear_4_216)"
+            fillOpacity="0.3"
+            transform="rotate(30 480 0)"
+          />
+          <rect
+            x="720"
+            y="0"
+            width="240"
+            height="1200"
+            fill="url(#paint4_linear_4_216)"
+            fillOpacity="0.3"
+            transform="rotate(30 720 0)"
+          />
+          <rect
+            x="960"
+            y="0"
+            width="240"
+            height="1200"
+            fill="url(#paint5_linear_4_216)"
+            fillOpacity="0.3"
+            transform="rotate(30 960 0)"
+          />
+          <rect
+            x="1200"
+            y="0"
+            width="240"
+            height="1200"
+            fill="url(#paint6_linear_4_216)"
+            fillOpacity="0.3"
+            transform="rotate(30 1200 0)"
+          />
+        </g>
 
-      {/* Gradiente radial para suavizar fundo */}
-      <div className="absolute inset-0 bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-transparent via-gray-100 to-white opacity-40" />
+        <defs>
+          <filter
+            id="filter0_f_4_216"
+            x="-480"
+            y="-240"
+            width="2160"
+            height="1680"
+            filterUnits="userSpaceOnUse"
+            colorInterpolationFilters="sRGB"
+          >
+            <feFlood floodOpacity="0" result="BackgroundImageFix" />
+            <feBlend
+              mode="normal"
+              in="SourceGraphic"
+              in2="BackgroundImageFix"
+              result="shape"
+            />
+            <feGaussianBlur stdDeviation="120" result="effect1_foregroundBlur_4_216" />
+          </filter>
 
-      <style jsx global>{`
-        @keyframes beamMove {
-          0% {
-            transform: translateX(-200%);
-          }
-          100% {
-            transform: translateX(200%);
-          }
-        }
-        .animate-beam-move {
-          animation: beamMove 8s linear infinite;
-        }
-      `}</style>
+          <linearGradient
+            id="paint0_linear_4_216"
+            x1="0"
+            y1="0"
+            x2="0"
+            y2="1200"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop stopColor="#00B7FF" />
+            <stop offset="1" stopColor="#5A2EB8" stopOpacity="0" />
+          </linearGradient>
+
+          <linearGradient
+            id="paint1_linear_4_216"
+            x1="0"
+            y1="0"
+            x2="0"
+            y2="1200"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop stopColor="#00B7FF" />
+            <stop offset="1" stopColor="#5A2EB8" stopOpacity="0" />
+          </linearGradient>
+
+          <linearGradient
+            id="paint2_linear_4_216"
+            x1="0"
+            y1="0"
+            x2="0"
+            y2="1200"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop stopColor="#00B7FF" />
+            <stop offset="1" stopColor="#5A2EB8" stopOpacity="0" />
+          </linearGradient>
+
+          <linearGradient
+            id="paint3_linear_4_216"
+            x1="0"
+            y1="0"
+            x2="0"
+            y2="1200"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop stopColor="#00B7FF" />
+            <stop offset="1" stopColor="#5A2EB8" stopOpacity="0" />
+          </linearGradient>
+
+          <linearGradient
+            id="paint4_linear_4_216"
+            x1="0"
+            y1="0"
+            x2="0"
+            y2="1200"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop stopColor="#00B7FF" />
+            <stop offset="1" stopColor="#5A2EB8" stopOpacity="0" />
+          </linearGradient>
+
+          <linearGradient
+            id="paint5_linear_4_216"
+            x1="0"
+            y1="0"
+            x2="0"
+            y2="1200"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop stopColor="#00B7FF" />
+            <stop offset="1" stopColor="#5A2EB8" stopOpacity="0" />
+          </linearGradient>
+
+          <linearGradient
+            id="paint6_linear_4_216"
+            x1="0"
+            y1="0"
+            x2="0"
+            y2="1200"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop stopColor="#00B7FF" />
+            <stop offset="1" stopColor="#5A2EB8" stopOpacity="0" />
+          </linearGradient>
+        </defs>
+      </svg>
     </div>
   );
 };
