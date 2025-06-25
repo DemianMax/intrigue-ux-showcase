3ea88c71c8ba44bca6954754576cc6a0b7c35d44
Max Demian <max3demian@gmail.com>
2025-06-25 18:06:27 -0300

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,19 +1,12 @@
-import ScrollDepthLayout from "@/components/ScrollDepthLayout";
-
 const Index = () => {
-  // Dados de teste super simples
-  const sections = [
-    <div key="hero" style={{ height: "100vh", background: "#fff", display: "flex", alignItems: "center", justifyContent: "center" }}>
-      <h1 style={{ fontSize: 40 }}>Teste Hero</h1>
-    </div>,
-    <div key="about" style={{ height: "100vh", background: "#dde", display: "flex", alignItems: "center", justifyContent: "center" }}>
-      <h1 style={{ fontSize: 40 }}>Teste About</h1>
-    </div>
-  ];
-
   return (
     <div>
-      <ScrollDepthLayout>{sections}</ScrollDepthLayout>
+      <div style={{ height: "100vh", background: "#fff", display: "flex", alignItems: "center", justifyContent: "center" }}>
+        <h1 style={{ fontSize: 40 }}>Teste Hero</h1>
+      </div>
+      <div style={{ height: "100vh", background: "#dde", display: "flex", alignItems: "center", justifyContent: "center" }}>
+        <h1 style={{ fontSize: 40 }}>Teste About</h1>
+      </div>
     </div>
   );
 };
