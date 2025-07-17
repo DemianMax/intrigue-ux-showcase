c1625de8b10e9a0c9304efa2efebfd801e1f9d08
Max Demian <max3demian@gmail.com>
2025-07-17 20:03:43 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <ThemeProvider defaultTheme="light" storageKey="lovable-ui-theme">
+    <ThemeProvider defaultTheme="dark" storageKey="lovable-ui-theme">
       <LanguageProvider>
         <TooltipProvider>
           <Toaster />
